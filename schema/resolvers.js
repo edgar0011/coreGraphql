40f41d4dec3f614a46618a6a7fb7441581716081
edgar0011 <martin.weiser@gmail.com>
2017-10-25 12:41:36 +0200

    refactor(): appollo -> graphql-express (FCB)

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -3,7 +3,17 @@ export default {
   Query: {
     hello: (parent, args, context) => {
       const de = parent + args + context;
-      return de ? `Hi ${args.name}` : 'Empty';
+      return new Promise((resolve, reject) => {
+        if (['hell', 'devil', 'evil', '666'].find(name => args.name.toLowerCase().indexOf(name) > -1)) {
+          setTimeout(() => {
+            reject(new Error(`Inappropriate name: ${args.name}`));
+          }, 1000);
+          return;
+        }
+        setTimeout(() => {
+          resolve(de ? `Hi ${args.name}` : 'Empty');
+        }, 1000);
+      });
     },
   },
 };
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
 import express from 'express';
-import bodyParser from 'body-parser';
-import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
+// import bodyParser from 'body-parser';
+// import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
+// is the SAME !!!!!!
+// import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+// AND equivalent to, which is better:
+import graphqlHTTP from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
-
 
 import typeDefs from './schema/typeDefs';
 import resolvers from './schema/resolvers';
@@ -16,10 +19,21 @@ const schema = makeExecutableSchema({
 const PORT = 8080;
 const app = express();
 
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: false }));
+app.use('/graphiql', graphqlHTTP({ schema, graphiql: true }));
+
+app.get('/', (req, res) => {
+  res.json({ result: 'ok' });
+});
+
+app.get('/hello', (req, res) => {
+  res.json({ result: 'Hi there...' });
+});
+
 
 app.listen(PORT, () => {
-  console.log('app runing at 8080');
+  console.log('app running at 8080');
 });
